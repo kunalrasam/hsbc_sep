0a8c38de9d0e5297fcdd88029c311bc20940acba
kunalrasam <kunalrasam4@gmail.com>
2020-09-22 17:48:25 +0530

    Create facebookuser.java

---
+++ facebookexamplewithFilehandling/fentity/facebookuser.java
@@ -0,0 +1,29 @@
+package com.first_java.fentity;
+
+public class facebookuser {
+	
+	
+	private String name;
+	
+	private String nname;
+	public String getname()
+	{
+		return name;
+	}
+	
+	public String getnewname()
+	{
+		return nname;
+	}
+	
+	public void SetName(String name)
+	{
+		this.name=name;
+	}
+	public void SetnewName(String name)
+	{
+		this.nname=name;
+	}
+	
+
+}
