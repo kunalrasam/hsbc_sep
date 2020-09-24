b0125908e24f3911766d3513f09c5501dc22098f
kunalrasam <kunalrasam4@gmail.com>
2020-09-24 17:46:08 +0530

    Create controllerinterface.java

---
+++ goodproducts/controller/controllerinterface.java
@@ -0,0 +1,14 @@
+package com.goodproducts.controller;
+
+import java.util.List;
+
+import com.goodproducts.entity.fooditems;
+
+public interface controllerinterface {
+	public void addcontrolleritems() throws Exception;
+	public void addapparelcontrolleritems();
+	public  void gettopcontrolleritems();
+	
+	
+
+}
